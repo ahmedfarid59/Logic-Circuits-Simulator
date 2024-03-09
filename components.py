d61a42eddd3f7d5111588ebfd2c9ad1ce285cb7c
unknown <ahmed.farid.592002@gmail.com>
2024-03-09 16:42:06 +0200

    added the classes definitions

---
+++ components.py
@@ -0,0 +1,14 @@
+class input:
+	def __init__(self,name,state):
+		self.name=name
+		self.state=state
+
+class gate:
+	def __init__(self,name,noi,expression,delay):
+		self.name=name
+		self.noi=noi
+		self.expression=expression
+		self.delay=delay
+        
+
+
