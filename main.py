f88da5378851a8b0c9c91e2800390a66a5052a21
unknown <ahmed.farid.592002@gmail.com>
2024-03-09 23:10:19 +0200

    for save

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from component import *
 
 file=open("lib.lib","r")
+inputs=components=[]
 
 for l in file:
 	lst=l.strip().split(",")
+	components.append(component(lst[0],int(lst[1]),lst[2],int(lst[3])))
 	print(lst)
 
