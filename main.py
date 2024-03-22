30134427a5bc3e337fa40e84a5aa652b07d264d4
Ahmed Farid <ahmed.farid.592002@gmail.com>
2024-03-22 10:08:26 +0200

    to try the new continuous integration action

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ print("starting updates")
 # reading the stim file and updating the variables after removing the whitespaces and extra line breaks and normalizing the cases
 with open("modules\\stim.stim","r" ) as stim:
 	for line in stim:
-
 		lst=line.strip().replace(" ","").upper().split(",")
 		d=int(lst[0])
 		variables[lst[1]].update(int(lst[2]),d)
